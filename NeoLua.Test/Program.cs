1f6d70cc86d5cd4f7f14eef38da0a9176af4c2fb
azraelrabbit <rock_82@outlook.com>
2017-08-15 23:53:29 +0800

    ported to .net starndard 2.0, only for Neolua and neoluaD

---
+++ NeoLua.Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NeoLua.Test.Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
